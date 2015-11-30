789a5b2fcac885cf8bbddef7480b1b3eaaa922ed
Max Alcala <malcala@shoppertrak.com>
2015-11-30 15:26:07 -0600

    working example

---
+++ README.md
@@ -0,0 +1,6 @@
+## Simple upload server example
+
+This is pretty much ripped from  the [multiparty examples](https://github.com/andrewrk/node-multiparty). This will receive a file in the upload_files directory automatically and send over an html form if you're hitting the root endpoint.
+
+## USAGE
+`node server.js --harmony`
--- server.js
+++ server.js
@@ -1,3 +1,30 @@
-import multiparty from 'multiparty';
-import http from 'http';
-import util from 'util';
+'use strict';
+const multiparty = require('multiparty');
+const http = require('http');
+const util = require('util');
+
+http.createServer((req, res) => {
+  if (req.url === '/upload' && req.method === 'POST') {
+    // parse a file upload
+    let form = new multiparty.Form({autoFiles: true, uploadDir: './upload_files'});
+
+    form.parse(req, (err, fields, files) => {
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      // Object.keys(files).forEach((f) => )
+      res.end(util.inspect({fields: fields, files: files}));
+    });
+
+    return;
+  }
+
+    // show a file upload form
+  res.writeHead(200, {'content-type': 'text/html'});
+  res.end(
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="text" name="title"><br>'+
+    '<input type="file" name="upload" multiple="multiple"><br>'+
+    '<input type="submit" value="Upload">'+
+    '</form>'
+  );
+}).listen(8081);
